4154b5f25c5874498bb9ba023d58ea466f1f699a
sm <smuzani@gmail.com>
2024-07-21 13:53:51 +0800

    migrate dependencies

--- randomuser/src/main/java/com/template/randomuser/screen/UsersScreen.kt
+++ randomuser/src/main/java/com/template/randomuser/screen/UsersScreen.kt
@@ -26,7 +26,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.rememberAsyncImagePainter
 import com.template.randomuser.network.RandomUser
 
