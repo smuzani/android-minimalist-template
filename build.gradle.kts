05391f9aa842f6bdfb9bc898299baad1b5f056fd
ainulaikha <ainulaikha@gmail.com>
2024-08-25 00:41:03 +0800

    randomuser build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,18 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    ext {
-        compose_version = '1.4.3'
+    extra.apply {
+        set("compose_version", "1.4.3")
     }
     repositories {
         google()
         mavenCentral()
     }
     dependencies {
-        classpath libs.gradle
-        classpath libs.kotlin.gradle.plugin
-        classpath libs.hilt.android.gradle.plugin
+        classpath(libs.gradle)
+        classpath(libs.kotlin.gradle.plugin)
+        classpath(libs.hilt.android.gradle.plugin)
 
         // NOTE: Do not place your application dependencies here; they belong
-        // in the individual module build.gradle files
+        // in the individual module build.gradle.kts files
     }
 }
